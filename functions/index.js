29cfce244fe5fa45fce81b47739ce8789ee7e280
innovandocondiego <innovandocondiego@gmail.com>
2025-06-18 22:51:16 -0500

    Primera muestra lista y funcional de DRUMMAP

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,4 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-
-admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("¡Hola desde Cloud Functions!");
